633e334b292830f7250014597218db7eb375850b
Stephen Olson <stephenolson@gmail.com>
2018-11-20 19:01:39 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,26 +1,29 @@
 class Artist
-  
   attr_accessor :name
-  @@all = [ ]
-  
+  @@all = []
+
   def initialize(name)
     @name = name
-    @songs = [ ]
+    @songs = []
     @@all << self
   end
-  
-  def songs 
+
+  def songs
     @songs
   end
-  
+
   def save
     @@all << self
+  end
+
+  def self.all
+    @@all
   end
 
   def add_song(song)
     @songs << song
   end
-  
+
   def self.find_or_create_by_name(name)
     if @@all.all? { |query| query.name != name}
       artist = self.new(name)
@@ -32,6 +35,4 @@ class Artist
   def print_songs
     @songs.each { |song| puts song.name }
   end
-  
-
 end
