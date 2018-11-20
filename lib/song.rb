81c875c5152e4daadc3fe370df4172ae95019485
Stephen Olson <stephenolson@gmail.com>
2018-11-20 19:00:09 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,7 +11,11 @@ class Song
       song = self.new(title)
       song.artist_name = filename.split(" - ")[0]
     song
+  end
   
+  def artist_name=(name)
+    @artist = Artist.find_or_create_by_name(name)
+    self.artist.add_song(name)
   end
 
 end
